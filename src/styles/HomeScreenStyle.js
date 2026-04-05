bd63d1e2bf001270ee0ac0b62fa5ba84cf4718f4
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 16:24:07 -0300

    feat: Add HomeScreenStyle

---
+++ src/styles/HomeScreenStyle.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Platform } from "react-native";
+import {
+	STATUS_BAR_HEIGHT,
+	PRIMARY_COLOR,
+	TEXT_COLOR_DARK,
+	TEXT_COLOR_WHITE,
+	TEXT_COLOR_DISABLED,
+	BACKGROUND_COLOR_LIGHT } from "../constants/style";
+
+const loaderStyles = {
+	flex: 1,
+	justifyContent: "center",
+	alignItems: "center"
+}
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: BACKGROUND_COLOR_LIGHT,
+		paddingTop: STATUS_BAR_HEIGHT
+	},
+	title: {
+		fontSize: 28,
+		fontWeight: 700,
+		textAlign: "center",
+		marginVertical: 20,
+		color: TEXT_COLOR_DARK
+	},
+
+	loader: loaderStyles,
+
+	listEmptyContainer: loaderStyles,
+
+	list: {
+		flex: 1,
+		marginHorizontal: 15
+	},
+	emptyText: {
+		fontSize: 16,
+		color: TEXT_COLOR_DISABLED
+	},
+	addButton: {
+		backgroundColor: PRIMARY_COLOR,
+		width: 60,
+		height: 60,
+		borderRadius: 30,
+		justifyContent: "center",
+		alignItems: "center",
+		position: "absolute",
+		bottom: 30,
+		right: 30,
+		elevation: 6
+	}
+});
