bd25ed782d3fbd48d7e68f167ddbb51c8e545476
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 16:25:28 -0300

    feat: Add TaskItemStyle

---
+++ src/styles/TaskItemStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+import {
+	PRIMARY_COLOR,
+	ACCENT_COLOR_YELLOW,
+	ACCENT_COLOR_RED,
+	TEXT_COLOR_DARK,
+	TEXT_COLOR_DISABLED,
+	BACKGROUND_COLOR_WHITE } from "../constants/style";
+
+export default StyleSheet.create({
+	taskItem: {
+		backgroundColor: BACKGROUND_COLOR_WHITE,
+		paddingVertical: 12,
+		paddingHorizontal: 15
+	},
+})
